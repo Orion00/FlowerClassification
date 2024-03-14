ee422f9520b174cbd536d2ac8ccc4e2c043b95d0
Orion Bowers <orionbowers@gmail.com>
2024-03-14 11:57:47 -0600

    Get NN predictions to work

--- predictions.py
+++ predictions.py
@@ -12,126 +12,12 @@ from sklearn.metrics import accuracy_score
 from sklearn.ensemble import RandomForestClassifier
 from PIL import Image
 from sklearn.model_selection import train_test_split
-
-### READ IN DATA
-# Neural Net Data
-# %%
-images_dir = "Data/training/training/"
-labels = pd.read_csv("Data/training_labels.csv")
-
-# add the directory to the filename
-labels['ID'] = labels['ID'].apply(lambda x: os.path.join(images_dir, x))
-
-# Initialize the ImageDataGenerator
-# You can change the size of the validation split (0.25 is 25% of data used as validation set)
-datagen = ImageDataGenerator(rescale=1./255, validation_split=0.25)
-
-# Create the training and validation generators
-train_generator = datagen.flow_from_dataframe(
-    dataframe=labels,
-    directory=None,  
-    x_col='ID',
-    y_col='target',
-    target_size=(224, 224), # You can change the size of the image
-    batch_size=32, # You can change the batch_size
-    class_mode='categorical',  
-    subset='training'
-)
-
-validation_generator = datagen.flow_from_dataframe(
-    dataframe=labels,
-    directory=None,  
-    x_col='ID',
-    y_col='target',
-    target_size=(224, 224), # Should match training size
-    batch_size=32, # Should match training
-    class_mode='categorical',  
-    subset='validation'
-)
-
-
-### PLOT FIRST FEW IMAGES
-# %%
-## Plot a few of the images
-import matplotlib.pyplot as plt
-
-# Fetch a batch of images and their labels
-images, labels = next(train_generator)
-
-# Number of images to show
-num_images = 8
-
-plt.figure(figsize=(20, 10))
-for i in range(num_images):
-    ax = plt.subplot(2, 4, i + 1)
-    plt.imshow(images[i])
-    # The label for current image
-    label_index = labels[i].argmax()  # Convert one-hot encoding to index
-    label = list(train_generator.class_indices.keys())[label_index]  # Get label name from index
-    plt.title(label)
-    plt.axis('off')
-plt.show()
-
-
-
-### NN Train Classifier
-# %%
-base_model = VGG16(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
-
-# Freeze all layers in the base model
-for layer in base_model.layers:
-    layer.trainable = False
-
-# Optionally, unfreeze the top N layers
-N = 5
-for layer in base_model.layers[-N:]:
-    layer.trainable = True
-
-# Add custom layers on top
-x = base_model.output
-x = Flatten()(x)
-x = Dense(512, activation='relu')(x)
-x = Dense(256, activation='relu')(x)
-predictions = Dense(5, activation='softmax')(x)
-
-model_transfer = Model(inputs=base_model.input, outputs=predictions)
-
-# Compile with a smaller learning rate
-model_transfer.compile(optimizer='adam',
-                       loss='categorical_crossentropy',
-                       metrics=['accuracy'])
-
-# %%
-model_transfer.fit(train_generator, validation_data=validation_generator, epochs=1)
-
-### SAVE/LOAD THE MODEL
-# %%
-# model_transfer.save('imageNet2Epoch.keras')
-# model_transfer = keras.models.load_model("imageNet2Epoch.keras")
-
-### Prediction
-# %%
-preds = model_transfer.predict(validation_generator)
-
-### Validation
-# %%
-accuracy_score(labels,preds)
-
-# %%
-preds = model_transfer.predict_generator(validation_generator, steps=len(validation_generator))
-
-
-# Get the class index with the highest probability for each image
-predicted_classes = np.argmax(preds, axis=1)
-
-# Now you have an array of predicted class indices for each image
-print(predicted_classes)
-
+from tensorflow.keras.models import Sequential
 
 
 ### Random Forest Classifier
 # RandomForest, Max Depth 8
-# Took 10(?) minutes to fit
+# Took 2ish minutes to fit
 # Train Accuracy 0.9348534201954397
 # Test (Validation) Accuracy 0.487698986975398
 
@@ -208,3 +94,164 @@ sub['Prediction'] = preds_final
 sub.to_csv("RandomForest10Depth.csv", index=False)
 
 
+
+
+
+
+
+#### Neural Net Data
+# 1 Epoch
+# Train Accuracy 0.20725588575839443
+# Test (Validation) Accuracy 0.20278099652375434
+
+# 5 Epochs
+# Train Accuracy 0.2068699343882671
+# Test (Validation) Accuracy 0.22479721900347624
+
+# %%
+images_dir = "Data/training/training/"
+labels = pd.read_csv("Data/training_labels.csv")
+
+# add the directory to the filename
+labels['ID'] = labels['ID'].apply(lambda x: os.path.join(images_dir, x))
+
+# Initialize the ImageDataGenerator
+datagen = ImageDataGenerator(rescale=1./255, validation_split=0.25)
+
+# Create the training and validation generators
+train_generator = datagen.flow_from_dataframe(
+    dataframe=labels,
+    directory=None,  
+    x_col='ID',
+    y_col='target',
+    target_size=(224, 224), # You can change the size of the image
+    batch_size=32, # You can change the batch_size
+    class_mode='categorical',  
+    subset='training',
+    shuffle=False
+)
+
+validation_generator = datagen.flow_from_dataframe(
+    dataframe=labels,
+    directory=None,  
+    x_col='ID',
+    y_col='target',
+    target_size=(224, 224), # Should match training size
+    batch_size=32, # Should match training
+    class_mode='categorical',  
+    subset='validation',
+    shuffle=False
+)
+
+# %%
+num_classes=5
+
+my_new_model = Sequential()
+#my_new_model.add(ResNet50(include_top=False, pooling='avg', weights=resnet_weights_path))
+my_new_model.add(VGG16(include_top=False, pooling='avg', weights='imagenet',input_shape=(224, 224, 3)))
+my_new_model.add(Dense(units=num_classes, activation='softmax'))
+ 
+my_new_model.layers[0].trainable = False
+my_new_model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# %%
+my_new_model.fit(
+        train_generator,
+        epochs=5,
+        validation_data=validation_generator,
+        workers=3,
+        validation_steps=1)
+# %%
+# my_new_model.save('imageNet5Epoch.keras')
+# my_new_model = keras.models.load_model("imageNet5Epoch.keras")
+
+
+# %%
+# train_preds = my_new_model.predict(train_generator)
+valid_preds = my_new_model.predict(validation_generator)
+
+
+
+### Validation
+# %%
+train_labels = train_generator.classes
+valid_labels = validation_generator.classes
+test_preds_labs = np.argmax(train_preds, axis=1)
+valid_preds_labs = np.argmax(valid_preds, axis=1)
+print("Train Accuracy",accuracy_score(train_labels,test_preds_labs))
+print("Test (Validation) Accuracy",accuracy_score(valid_labels,valid_preds_labs))
+
+
+train_eval = my_new_model.evaluate(train_generator)
+validation_eval = my_new_model.evaluate(validation_generator)
+# %%
+### Predictions
+test_images_dir = "Data/testing/testing/"
+test_image_paths = [f for f in os.listdir(test_images_dir)]
+sub = pd.DataFrame(columns=["ID","Prediction"])
+sub['ID'] = test_image_paths
+sub['ID'] = sub['ID'].apply(lambda x: os.path.join(test_images_dir, x))
+
+# Initialize the ImageDataGenerator
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+# Create the training and validation generators
+test_generator = test_datagen.flow_from_dataframe(
+    dataframe=sub,
+    directory=None,  
+    x_col='ID',
+    target_size=(224, 224),
+    class_mode=None,  
+    validate_filenames=False,
+    shuffle=False
+)
+
+# %%
+test_preds = my_new_model.predict(test_generator)
+
+# %%
+class_names = list(train_generator.class_indices.keys())
+sub['Prediction'] = np.argmax(test_preds, axis=-1)
+sub['Prediction'] = [class_names[label] for label in sub['Prediction']]
+sub['ID'] = [i[21:] for i in sub['ID']]
+
+sub.to_csv("VG116Epoch5.csv", index=False)
+
+
+
+
+
+### NN Train Classifier
+# %%
+base_model = VGG16(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
+
+# Freeze all layers in the base model
+for layer in base_model.layers:
+    layer.trainable = False
+
+# Optionally, unfreeze the top N layers
+N = 5
+for layer in base_model.layers[-N:]:
+    layer.trainable = True
+
+# Add custom layers on top
+x = base_model.output
+x = Flatten()(x)
+x = Dense(512, activation='relu')(x)
+x = Dense(256, activation='relu')(x)
+predictions = Dense(5, activation='softmax')(x)
+
+model_transfer = Model(inputs=base_model.input, outputs=predictions)
+
+# Compile with a smaller learning rate
+model_transfer.compile(optimizer='adam',
+                       loss='categorical_crossentropy',
+                       metrics=['accuracy'])
+
+# %%
+model_transfer.fit(train_generator, validation_data=validation_generator, epochs=1)
+
+### SAVE/LOAD THE MODEL
+# %%
+# model_transfer.save('imageNet2Epoch.keras')
+# model_transfer = keras.models.load_model("imageNet2Epoch.keras")
